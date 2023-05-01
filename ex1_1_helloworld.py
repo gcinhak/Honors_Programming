b3817a6d471b81ab9edf7b99da578965bddbf545
inhak <gcinhak@gmail.com>
2023-05-01 12:42:41 +0900

    test

--- ex1_1_helloworld.py
+++ ex1_1_helloworld.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QApplication, QLabel
 
 app = QApplication(sys.argv)
 
-label = QLabel("Hello World")
+label = QLabel("Hello World!!")
 label.show()
 
 sys.exit(app.exec_())
