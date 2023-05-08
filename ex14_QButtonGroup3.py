4e78150d0ffe1a76d146a9774a2bb8db54eabd04
Inhak Kim <gcinhak@gmail.com>
2023-05-08 13:44:39 +0900

    test

--- ex14_QButtonGroup3.py
+++ ex14_QButtonGroup3.py
@@ -19,9 +19,6 @@ class MainWindow(QMainWindow):
         self.t_btn_group.addButton(t_btn2, 2)
         self.t_btn_group.addButton(t_btn3, 3)
         self.t_btn_group.addButton(t_btn4, 4)
-        self.t_btn_group.setExclusive(False) # 여러 버튼 선택 가능 하도록
-        # self.t_btn_group.removeButton(t_btn1)
-        # self.t_btn_group.removeButton(t_btn2)
 
         g_btn1 = QRadioButton("10th")
         g_btn2 = QRadioButton("11th")
@@ -31,6 +28,10 @@ class MainWindow(QMainWindow):
         self.g_btn_group.addButton(g_btn1, 1)
         self.g_btn_group.addButton(g_btn2, 2)
         self.g_btn_group.addButton(g_btn3, 3)
+
+        # connect the signal to the slot
+        self.t_btn_group.buttonClicked.connect(self.on_clicked_t_btn)
+        self.g_btn_group.buttonClicked.connect(self.on_clicked_g_btn)
 
         #레이아웃 설정
         hbox = QHBoxLayout()
@@ -52,6 +53,13 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
         self.setGeometry(300, 300, 300, 100)
         self.setWindowTitle("GButtonGroup")
+
+    def on_clicked_t_btn(self):
+        print("Button Label is", self.t_btn_group.checkedButton().text(), \
+              ", id is:", self.t_btn_group.id(self.t_btn_group.checkedButton()))
+
+    def on_clicked_g_btn(self, obj):
+        print("Button Label is", obj.text(), ", id is:", self.g_btn_group.id(obj))
 
 if __name__ == '__main__':
     app = QApplication([])
