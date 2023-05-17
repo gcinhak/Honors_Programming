271eddc99bdfa65ffc378474ab83f9b0e1241d5d
inhak <gcinhak@gmail.com>
2023-05-18 08:42:02 +0900

    test

--- Examples/ex01_2_event_loop.py
+++ Examples/ex01_2_event_loop.py
@@ -9,5 +9,9 @@ label = QLabel("Hello World")
 label.show()
 
 print("Before event loop")
-sys.exit(app.exec_())
+# 시스템을 바로 종료
+# sys.exit(app.exec_())
+
+# 이벤트 루프를 종료 하고, 코드 진행
+app.exec_()
 print("After event loop")
