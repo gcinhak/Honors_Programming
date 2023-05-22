17cf540eb222a286d3464003d30a89122529fb8b
Inhak Kim <gcinhak@gmail.com>
2023-05-22 13:41:37 +0900

    test

--- Examples/ex14_QButtonGroup3.py
+++ Examples/ex14_QButtonGroup3.py
@@ -8,41 +8,41 @@ class MainWindow(QMainWindow):
         self.init_ui()
 
     def init_ui(self):
-        t_btn1 = QRadioButton("IT-AI")
-        t_btn2 = QRadioButton("BIO")
-        t_btn3 = QRadioButton("Design")
-        t_btn4 = QRadioButton("sports")
+        btn_itai = QRadioButton("IT-AI")
+        btn_bio = QRadioButton("BIO")
+        btn_design = QRadioButton("Design")
+        btn_sports = QRadioButton("Sports")
 
-        self.t_btn_group = QButtonGroup()
-        self.t_btn_group.addButton(t_btn1, 1)
-        self.t_btn_group.addButton(t_btn2, 2)
-        self.t_btn_group.addButton(t_btn3, 3)
-        self.t_btn_group.addButton(t_btn4, 4)
+        self.btn_group_track = QButtonGroup()
+        self.btn_group_track.addButton(btn_itai, 1)
+        self.btn_group_track.addButton(btn_bio, 2)
+        self.btn_group_track.addButton(btn_design, 3)
+        self.btn_group_track.addButton(btn_sports, 4)
 
-        g_btn1 = QRadioButton("10th")
-        g_btn2 = QRadioButton("11th")
-        g_btn3 = QRadioButton("12th")
+        btn_10 = QRadioButton("10th")
+        btn_11 = QRadioButton("11th")
+        btn_12 = QRadioButton("12th")
 
-        self.g_btn_group = QButtonGroup()
-        self.g_btn_group.addButton(g_btn1, 1)
-        self.g_btn_group.addButton(g_btn2, 2)
-        self.g_btn_group.addButton(g_btn3, 3)
+        self.btn_group_grade = QButtonGroup()
+        self.btn_group_grade.addButton(btn_10) # id: -2
+        self.btn_group_grade.addButton(btn_11) # id: -3
+        self.btn_group_grade.addButton(btn_12) # id: -4
 
         # connect the signal to the slot
-        self.t_btn_group.buttonClicked.connect(self.on_clicked_t_btn)
-        self.g_btn_group.buttonClicked.connect(self.on_clicked_g_btn)
+        self.btn_group_track.buttonClicked.connect(self.on_btn_track_clicked)
+        self.btn_group_grade.buttonClicked.connect(self.on_btn_grade_clicked)
 
         #레이아웃 설정
         hbox = QHBoxLayout()
         vbox1 = QVBoxLayout()
         vbox2 = QVBoxLayout()
-        vbox1.addWidget(t_btn1)
-        vbox1.addWidget(t_btn2)
-        vbox1.addWidget(t_btn3)
-        vbox1.addWidget(t_btn4)
-        vbox2.addWidget(g_btn1)
-        vbox2.addWidget(g_btn2)
-        vbox2.addWidget(g_btn3)
+        vbox1.addWidget(btn_itai)
+        vbox1.addWidget(btn_bio)
+        vbox1.addWidget(btn_design)
+        vbox1.addWidget(btn_sports)
+        vbox2.addWidget(btn_10)
+        vbox2.addWidget(btn_11)
+        vbox2.addWidget(btn_12)
         hbox.addLayout(vbox1)
         hbox.addLayout(vbox2)
 
@@ -53,11 +53,11 @@ class MainWindow(QMainWindow):
         self.setGeometry(300, 300, 300, 100)
         self.setWindowTitle("GButtonGroup")
 
-    def on_clicked_t_btn(self):
-        print("Button Label is", self.t_btn_group.checkedButton().text(), ", id is:", self.t_btn_group.id(self.t_btn_group.checkedButton()))
+    def on_btn_track_clicked(self):
+        print("Button Label is", self.btn_group_track.checkedButton().text(), ", id is:", self.btn_group_track.id(self.btn_group_track.checkedButton()))
 
-    def on_clicked_g_btn(self, obj):
-        print("Button Label is", obj.text(), ", id is:", self.g_btn_group.id(obj))
+    def on_btn_grade_clicked(self, obj):
+        print("Button Label is", obj.text(), ", id is:", self.btn_group_grade.id(obj))
 
 if __name__ == '__main__':
     app = QApplication([])
