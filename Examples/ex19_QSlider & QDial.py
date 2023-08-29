de1d60acb8f2fc18745235e7adc567abd860e58c
Inhak Kim <gcinhak@gmail.com>
2023-08-29 10:46:49 +0900

    test

--- Examples/ex19_QSlider & QDial.py
+++ Examples/ex19_QSlider & QDial.py
@@ -4,8 +4,6 @@ from PyQt5.QtCore import Qt, QDateTime
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QHBoxLayout, QSlider, \
     QVBoxLayout, QLabel, QDial, QSizePolicy
 
-
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -17,13 +15,15 @@ class MainWindow(QMainWindow):
         self.slider_horizontal.setRange(0, 50)
         self.slider_horizontal.setSingleStep(1)
         self.slider_horizontal.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.slider_horizontal.setTickPosition(3)
 
         # slider 설정
         self.slider_vertical = QSlider(Qt.Vertical)
-        self.slider_vertical.setRange(0, 50)
+        self.slider_vertical.setRange(0, 100)
         self.slider_vertical.setSingleStep(2)
         self.slider_vertical.setValue(50)
         self.slider_vertical.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
+        self.slider_vertical.setTickPosition(QSlider.TicksLeft or QSlider.TicksRight)
 
         # dail 설정
         self.dial = QDial(self)
@@ -34,16 +34,19 @@ class MainWindow(QMainWindow):
 
         # Label, Button 설정
         self.label_silder_horizontal = QLabel("Horizontal Value")
-        self.label_silder_horizontal.setFixedWidth(100)
+        self.label_silder_horizontal.setFixedWidth(150)
         self.label_silder_horizontal.setAlignment(Qt.AlignCenter)
+
         self.label_silder_vertical = QLabel("Vertical Value")
         self.label_silder_vertical.setFixedWidth(100)
         self.label_silder_vertical.setAlignment(Qt.AlignCenter)
+
         self.label_dial = QLabel("Dial Value")
         self.label_dial.setFixedWidth(100)
         self.label_dial.setAlignment(Qt.AlignCenter)
+
         btn_reset = QPushButton('reset')
-        btn_reset.setFixedHeight(70)
+        btn_reset.setFixedHeight(50)
 
         # 시그널 사용
         self.slider_horizontal.valueChanged.connect(self.hslider_changed)
@@ -69,16 +72,16 @@ class MainWindow(QMainWindow):
         widget.setLayout(vbox)
         self.setCentralWidget(widget)
         self.setWindowTitle('QSlider & QDial')
-        self.setGeometry(1000, 300, 800, 500)
+        self.setGeometry(1000, 300, 800, 400)
 
 
     def on_btn_reset_clicked(self):
         print(self.slider_horizontal.value())
         print(self.slider_vertical.value())
         print(self.dial.value())
-        self.slider_horizontal.setValue(0)
+        self.slider_horizontal.setValue(10)
         self.slider_vertical.setValue(50)
-        self.dial.setValue(0)
+        self.dial.setValue(30)
 
     def hslider_changed(self):
         self.label_silder_horizontal.setText(str(self.slider_horizontal.value()))
---
+++ Examples/ex20_QComboBox2.py
@@ -0,0 +1,83 @@
+import sys
+
+from PyQt5.QtCore import Qt, QDateTime
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, \
+    QSizePolicy, QComboBox, QLineEdit
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        # 콤보박스 생성 및 세팅
+        self.cb = QComboBox()
+        lineEdit = QLineEdit()
+        self.cb.setLineEdit(lineEdit)
+        self.cb.addItem('Option1')
+        self.cb.addItem('Option3')
+        self.cb.addItems(['Option4','Option5'])
+        self.cb.insertItem(1, 'Option2')
+        # self.cb.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        # 버튼, 라벨 생성 및 세팅
+        self.lb = QLabel()
+        btn = QPushButton("btn1")
+        btn_add = QPushButton('AddItem')
+        self.le = QLineEdit()
+        # btn.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        # 시그널 설정
+        # self.cb.activated.connect(self.changed_item)
+        # self.cb.currentIndexChanged.connect(self.changed_item)
+        # self.cb.currentTextChanged.connect(self.changed_item)
+        # self.cb.highlighted.connect(self.changed_item)
+        # self.cb.textActivated.connect(self.changed_item)
+        # self.cb.textHighlighted.connect(self.changed_item)
+        self.cb.editTextChanged.connect(self.changed_item)
+
+        btn.clicked.connect(self.on_clicked_btn)
+        btn_add.clicked.connect(self.on_clicked_btn_add)
+
+        # 레이아웃 설정
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.lb)
+        vbox.addWidget(self.cb)
+        vbox.addWidget(btn)
+        vbox.addWidget(self.le)
+        vbox.addWidget(btn_add)
+
+        datetime = QDateTime.currentDateTime()
+        self.statusBar().showMessage(datetime.toString('yyyy-MM-dd, hh:mm:ss'))
+
+        # 윈도우 설정
+        widget = QWidget()
+        widget.setLayout(vbox)
+        self.setCentralWidget(widget)
+        self.setWindowTitle('ComboBox')
+        self.setGeometry(800, 300, 150, 100)
+
+    # 슬롯
+    def changed_item(self, i):
+        print("changed item:",i)
+
+    def on_clicked_btn_add(self):
+        self.cb.addItem(self.le.text())
+
+    def on_clicked_btn(self):
+        # self.lb.setText("현재 index: "+str(self.cb.currentIndex()))
+        # self.lb.setText("현재 text: "+self.cb.currentText())
+        # self.lb.setText("항목 개수: "+str(self.cb.count()))
+        # self.lb.setText("2번째 항목은: "+str(self.cb.itemText(1)))
+        # self.cb.removeItem(self.cb.currentIndex())
+        # self.cb.clear()
+
+        self.cb.setCurrentIndex(0)
+        # self.cb.setCurrentText('Option4')
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
